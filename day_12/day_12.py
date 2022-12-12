f096a31230df393e3b4ee354ff5fb385f8edd80b
megalonyx <d@nieltiggemann.de>
2022-12-12 07:58:34 +0100

    read data.

--- day_12/day_12.py
+++ day_12/day_12.py
@@ -1,16 +1,57 @@
 #!/usr/bin/env python3
 
+raw_data = []
+heightmap = []
+size_x = 0
+size_y = 0
+start_x = 0
+start_y = 0
+end_x = 0
+end_y = 0
+
 def parse_line(line):
-    pass
+    global raw_data
+    raw_data.append(line)
 
 def parse_file(filename):
     with open(filename, 'r') as f:
         for line in f:
             parse_line(line.strip('\n'))
 
+def build_heightmap():
+    global heightmap, size_x, size_y, start_x, start_y, end_x, end_y
+    size_y = len(raw_data)
+    size_x = len(raw_data[0])
+    heightmap = [ [0 for _ in range(size_y)] for _ in range(size_x)]  # transposed to raw_data
+    for y in range(size_y):
+        for x in range(size_x):
+            c = raw_data[y][x]
+            if c == 'S':
+                start_x = x
+                start_y = y
+                heightmap[x][y] = ord('a') - ord('a')
+            elif c == 'E':
+                end_x = x
+                end_y = y
+                heightmap[x][y] = ord('z') - ord('a')
+            elif 'a' <= c <= 'z':
+                heightmap[x][y] = ord(c) - ord('a')
+            else:
+                print('error')
+                exit
+                
+def print_heightmap():
+    for y in range(size_y):
+        for x in range(size_x):
+            print(chr(heightmap[x][y]+ord('a')), end='')
+        print()
+
+
 def main():
-    parse_file('test.txt')
-#    parse_file('input.txt')
+#    parse_file('test.txt')
+    parse_file('input.txt')
+    build_heightmap()
+#    print_heightmap()
 
 if __name__ == '__main__':
     main()
